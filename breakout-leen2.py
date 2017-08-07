72d3c92d9b982fba457bcf2443baf9ce21ae92dc
itamar19-meet <itamar19@meet.mit.edu>
2017-08-07 22:28:16 +0300

    Stopping here

--- breakout-leen2.py
+++ breakout-leen2.py
@@ -22,19 +22,23 @@ stamp_list=[]
 obj = turtle.clone()
 obj.showturtle()
 obj.shape("square")
-obj.goto(0, -250)
-clones_list = [obj]
+obj.resizemode('user')
+obj.shapesize(1,LENGHT,1)
+platform_height = -SCREEN_HEIGHT/2 + 50
+obj.goto(0, platform_height)
+
+##clones_list = [obj]
 
 
-for n in range(LENGHT):
-    last_pos = clones_list[-1].pos()
-    last_x = last_pos[0]
-    new_x = last_x + SQUARE_SIZE
-    obj = turtle.clone()
-    obj.showturtle()
-    obj.shape("square")
-    obj.goto(new_x, last_pos[1])
-    clones_list.append(obj)
+##for n in range(LENGHT):
+##    last_pos = clones_list[-1].pos()
+##    last_x = last_pos[0]
+##    new_x = last_x + SQUARE_SIZE
+##    obj = turtle.clone()
+##    obj.showturtle()
+##    obj.shape("square")
+##    obj.goto(new_x, last_pos[1])
+##    clones_list.append(obj)
     
     
 
@@ -50,49 +54,63 @@ for n in range(LENGHT):
     
 LEFT_ARROW = 'Left'
 RIGHT_ARROW= 'Right'
-
+LEFT_EDGE = -SCREEN_WIDTH/2
+RIGHT_EDGE = SCREEN_WIDTH/2
 
 
 LEFT=0
 RIGHT=1
-
+SPEED = 3
 direction=LEFT
 
 def left():
     global direction
     direction= LEFT
-    move_plat()
+    if obj.pos()[0]-LENGHT/2*SQUARE_SIZE >= LEFT_EDGE:
+        obj.goto(obj.pos()[0]-SPEED*SQUARE_SIZE,obj.pos()[1])
     print("you pressed the left key!")
 
 def right():
     global direction
     direction= RIGHT
-    move_plat()
+    if obj.pos()[0]+LENGHT/2*SQUARE_SIZE <= RIGHT_EDGE:
+        obj.goto(obj.pos()[0]+SPEED*SQUARE_SIZE,obj.pos()[1])
     print("you pressed the right key!")
 
 turtle.onkeypress(left, LEFT_ARROW)
 turtle.onkeypress(right, RIGHT_ARROW)
 turtle.listen()
 
-def move_plat():
-    global clones_list
-    amount = 1
-    if direction==LEFT:
-        print(clones_list)
-        for obj in clones_list:
-            old_pos = obj.pos()
-            obj.goto(old_pos[0] - (amount*SQUARE_SIZE), old_pos[1])
-        print("you moved left!")
-        #time.sleep(.1)
+# Collision is going to be detected when:
+# - y_pos == platform_height+SQUARE_SIZE/2
+# - x_pos >= obj.pos()[0] - LENGHT/2*SQUARE_SIZE
+# - x_pos <= obj.pos()[0] + LENGHT/2*SQUARE_SIZE
 
-    elif direction==RIGHT:
-        for obj in clones_list:
-            print(obj.pos())
-            old_pos = obj.pos()
-            obj.goto(old_pos[0] + (amount*SQUARE_SIZE), old_pos[1])
-            print(obj.pos())
-        print("you moved right!")
-        #time.sleep(0.1)
+
+##def move_plat():
+##    global clones_list
+##    amount = 1
+##    if direction==LEFT:
+##        print(clones_list)
+##        back = clones_list[-1]
+##        front = clones_list[0]
+##        new_pos = front.pos()
+##        back.goto(new_pos[0]-(amount*SQUARE_SIZE),new_pos[1])
+##        clones_list = [back] + clones_list[0:-1]
+####        for obj in clones_list:
+####            old_pos = obj.pos()
+####            obj.goto(old_pos[0] - (amount*SQUARE_SIZE), old_pos[1])
+##        print("you moved left!")
+##        #time.sleep(.1)
+
+##    elif direction==RIGHT:
+##        for obj in clones_list:
+##            print(obj.pos())
+##            old_pos = obj.pos()
+##            obj.goto(old_pos[0] + (amount*SQUARE_SIZE), old_pos[1])
+##            print(obj.pos())
+##        print("you moved right!")
+##        #time.sleep(0.1)
     
 ##    global direction
 ##    my_pos=plat.pos()
